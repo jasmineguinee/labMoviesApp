948ee3e39b74a3915727ebed5c9f4ffa95762d55
Jasmine Guinee <jasmineguinee6@gmail.com>
2026-06-12 15:44:33 +0100

    Movie Details Page -included production countries.

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -26,7 +26,13 @@ export interface BaseMovieProps {
       id: number;
       name: string;
     }[];
+    
+    production_countries: {
+      iso_3166_1: string;
+      name: string;
+    }[];
   }
+
 
   export interface MovieImage {
   file_path: string;
